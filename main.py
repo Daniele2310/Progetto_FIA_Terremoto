1f970017d1568366581c815714e95b70f3ca2fbb
Erika <erikaoliveri92@gmail.com>
2026-03-27 17:48:09 +0100

    Integrazione analisi qualità dati di train e test nella pipeline di preprocessing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Script principale per il preprocessing dati - Terremoto Nepal 2015
 import pandas as pd
 from DataPreprocessing.puliziaASCII import PuliziaASCII, COLONNE_CATEGORICHE
 from DataPreprocessing.missingValues import MissingValuesHandler
+from DataPreprocessing.data_cleaning import DataQualityHandler
 
 
 def main():
@@ -16,15 +17,23 @@ def main():
     train_values, train_labels, test_values = pulizia.processa(
         colonne_categoriche=COLONNE_CATEGORICHE
     )
-    
+
+    # analisi training set
+    train_quality_handler = DataQualityHandler(train_values)
+    train_quality_report = train_quality_handler.esegui_controlli(plot=False)
+
+    # analisi del test set
+    test_quality_handler = DataQualityHandler(test_values)
+    test_quality_report = test_quality_handler.esegui_controlli(plot=False)
+
     df_merged = pd.merge(train_values, train_labels, on='building_id')
     
     handler = MissingValuesHandler(null_threshold=70)
     report = handler.analizza(df_merged, target_col='damage_grade')
 
     
-    return train_values, train_labels, test_values, report
+    return train_values, train_labels, test_values, report, train_quality_report, test_quality_report
 
 
 if __name__ == '__main__':
-    train_values, train_labels, test_values, report = main()
+    train_values, train_labels, test_values, report, train_quality_report, test_quality_report = main()
